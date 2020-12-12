eb7161f4b4e6fa756a9e2168b79b0209df262731
Lokesh Sharma <lokesh.sharma@tarento.com>
2020-12-12 10:21:39 +0530

    Structure Ready with login and register

---
+++ LS_HW_eCOM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LS_HW_eCOM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
